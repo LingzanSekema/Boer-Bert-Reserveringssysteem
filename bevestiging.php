a3358e5cb5cacf6568de83b2c509c1ca8d9de4c8
NiekEmmelot <niek.emmelot@student.hu.nl>
2024-12-02 09:23:17 +0100

    Bevestiging reserveringspagina

--- bevestiging.php
+++ bevestiging.php
@@ -7,6 +7,7 @@ $accommodation_types = [
 ];
 
 ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -22,10 +23,9 @@ $accommodation_types = [
         <div class="content">
             <div class="check">
                 ✔
-                </div>
-                <h1 class="header">Bedankt voor uw reservering!</h1>
-                <p>Wij hebben uw reservering in goede orde ontvangen. Hieronder vindt u een overzicht van uw gemaakte reservering.</p>
-
+            </div>
+            <h1 class="header">Bedankt voor uw reservering!</h1>
+            <p>Wij hebben uw reservering in goede orde ontvangen. Hieronder vindt u een overzicht van uw gemaakte reservering.</p>
 
             <div class="overzicht-reservering">
                 <table>
@@ -57,31 +57,10 @@ $accommodation_types = [
                         <th>Aantal Personen:</th>
                         <td><?php echo htmlspecialchars($_GET['guest_count']); ?></td>
                     </tr>
-                    
                 </table>
             </div>
-
-    <main>
-        <section class="content">
-            <h2>Bedankt voor je reservering!</h2>
-            <p>Hier zijn je gegevens:</p>
-            <ul>
-                <li><strong>Voornaam:</strong> <?php echo htmlspecialchars($_GET['firstname']); ?></li>
-                <li><strong>Achternaam:</strong> <?php echo htmlspecialchars($_GET['lastname']); ?></li>
-                <li><strong>Telefoonnummer:</strong> <?php echo htmlspecialchars($_GET['phone']); ?></li>
-                <li><strong>E-mail:</strong> <?php echo htmlspecialchars($_GET['email']); ?></li>
-                <li><strong>Check-in Datum:</strong> <?php echo htmlspecialchars($_GET['check_in']); ?></li>
-                <li><strong>Check-out Datum:</strong> <?php echo htmlspecialchars($_GET['check_out']); ?></li>
-                <li><strong>Aantal Personen:</strong> <?php echo htmlspecialchars($_GET['guest_count']); ?></li>
-                <li><strong>Accommodatie Type:</strong> <?php echo ($accommodation_types[$_GET['accommodation_type']]); ?></li>
-            </ul>
-            <p>We kijken ernaar uit je te verwelkomen op Camping De Groene Weide!</p>
-        </section>
-    </main>
-
-            <p class="Wekijkenernauit">We kijken ernaar uit om u te verwelkomen op Camping De Groene Weide!</p>
         </div>
-</div>
+    </div>
 </body>
 
 </html>
