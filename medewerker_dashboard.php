a9a30e8b00c2054d862246c386c0103e6f996c24
NiekEmmelot <niek.emmelot@student.hu.nl>
2024-12-03 13:47:44 +0100

    Overige veranderingen back-end

--- medewerker_dashboard.php
+++ medewerker_dashboard.php
@@ -1,73 +1,80 @@
 <?php
 session_start();
+require 'database.php'; // Zorg ervoor dat deze verbinding correct is ingesteld
 
-if (!isset($_SESSION['email']) || $_SESSION['role'] !== 'medewerker') {
-    // Gebruiker is niet ingelogd, doorverwijzen naar loginpagina
-    header('Location: login.php');
-    exit();
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // SQL om gebruiker te zoeken op e-mail
+    $sql = "SELECT * FROM users WHERE email = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+
+    if ($stmt) {
+        // Bind parameters en voer de query uit
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        // Controleer of gebruiker bestaat
+        if ($result && mysqli_num_rows($result) > 0) {
+            $data_uit_database = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+            // Controleer of wachtwoord klopt
+            if (password_verify($password, $data_uit_database['password'])) {
+                // Sla gegevens op in de sessie
+                $_SESSION['email'] = $data_uit_database['email'];
+                $_SESSION['role'] = $data_uit_database['role'];
+
+                // Redirect op basis van de rol
+                switch ($data_uit_database['role']) {
+                    case 'bert':
+                        $location = "bert_dashboard.php";
+                        break;
+                    case 'medewerker':
+                        $location = "medewerker_dashboard.php";
+                        break;
+                    default:
+                        $location = "reservering.php";
+                        break;
+                }
+
+                header("Location: $location");
+                exit;
+            } else {
+                $error = "Onjuist wachtwoord.";
+            }
+        } else {
+            $error = "Gebruiker met dit e-mailadres bestaat niet.";
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        $error = "Er is een fout opgetreden bij het voorbereiden van de query.";
+    }
+
+    mysqli_close($conn);
 }
-
-
-require 'database.php';
-
-$allReserveringen = $conn->query("SELECT * FROM reservering");
-$reserveringen = $allReserveringen->fetch_all(MYSQLI_ASSOC);
-
-// Log uit functionaliteit
-if (isset($_POST['logout'])) {
-    // Verwijder alle sessievariabelen
-    session_unset();
-    // Vernietig de sessie
-    session_destroy();
-    // Redirect naar de loginpagina
-    header('Location: login.php');
-    exit();
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Medewerker dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <title>Login</title>
 </head>
 
 <body>
-
-    <h1 class="mb-3 ">Medewerker dashboard</h1>
-    <div class="container">
-
-        <div class="row">
-
-            <?php foreach ($reserveringen as $reservering) { ?>
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                            <div class="card-body" style="background-color: #85C496;">
-                            <h5 class="card-title"><?php echo ($reservering["firstname"]) . ' ' . ($reservering["lastname"]); ?></h5>
-                            <p class="card-text"><strong>Check in:</strong> <?php echo ($reservering["check_in"]); ?></p>
-                            <p class="card-text"><strong>Check out:</strong> <?php echo ($reservering["check_out"]); ?></p>
-                            <p class="card-text"><strong>Aantal personen:</strong> <?php echo ($reservering["guest_count"]); ?></p>
-                            <p class="card-text"><strong>Accommodatie type:</strong> <?php echo ($reservering["accommodation_type"]); ?></p>
-                            <!--knop toevoegen -->
-                            <a href="info.php?id=<?php echo $reservering['id']; ?>" class="btn btn-light mt-2">Beheren</a>
-
-                        </div>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-<!-- Log uit knop -->
     <form method="POST" action="">
-        <button type="submit" name="logout" class="btn btn-danger">Log uit</button>
+        <input type="email" name="email" placeholder="E-mailadres" required>
+        <input type="password" name="password" placeholder="Wachtwoord" required>
+        <button type="submit" name="submit">Log in</button>
     </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <?php if (isset($error)) {
+        echo "<p style='color:red;'>$error</p>";
+    } ?>
 </body>
 
 </html>
